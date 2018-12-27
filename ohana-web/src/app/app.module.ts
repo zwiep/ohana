cfef39761703fca32187cf9b9ff7a06d43206b9c
geertmaes <geert.maes@cegeka.com>
2018-12-27 19:19:40 +0100

    - extracted search component for overview - added add person button as entry point for back-end developement

--- ohana-web/src/app/app.module.ts
+++ ohana-web/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import { HeaderComponent } from './header/header.component';
-import { OverviewComponent } from './overview/overview.component';
+import {HeaderComponent} from './header/header.component';
+import {OverviewComponent} from './overview/overview.component';
 import {MatButtonModule, MatFormFieldModule, MatInputModule} from "@angular/material";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {OverviewSearchComponent} from './overview/overview-search/overview-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     HeaderComponent,
-    OverviewComponent
+    OverviewComponent,
+    OverviewSearchComponent
   ],
   imports: [
     BrowserModule,
@@ -27,4 +28,5 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ ohana-web/src/app/overview/overview-search/overview-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overview-search',
+  templateUrl: './overview-search.component.html',
+  styleUrls: ['./overview-search.component.scss']
+})
+export class OverviewSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
