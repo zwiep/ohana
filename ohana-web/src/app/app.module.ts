2971aab3bfebafd7625c84229b7a0fca25a7f62b
Geert Maes <geert.maes@cegeka.com>
2018-12-27 16:41:43 +0100

    added overview component

--- ohana-web/src/app/app.module.ts
+++ ohana-web/src/app/app.module.ts
@@ -5,12 +5,14 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    HeaderComponent
+    HeaderComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
