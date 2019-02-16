b401443c768ae7182dc49034cc2b063850266cad
zwiep <geert.maes@cegeka.com>
2019-02-16 20:23:30 +0100

    - gradle structure rework - changed commit author

---
+++ ohana-api/src/main/java/be/mabi/ohana/Person.java
@@ -0,0 +1,19 @@
+package be.mabi.ohana;
+
+import java.time.Instant;
+
+public class Person {
+    String id;
+    String lastName;
+    String middleName;
+    String firstName;
+    String street;
+    String houseNumber;
+    String postalCode;
+    String City;
+    String Country;
+    String email;
+    String phoneNumber;
+    String mobileNumber;
+    Instant birthDate;
+}
